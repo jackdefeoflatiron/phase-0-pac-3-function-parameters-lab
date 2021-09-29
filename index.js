12317893254f85d4d1956e07f2b93842846c6fe5
John DeFeo <jackd3243@gmail.com>
2021-09-28 20:40:58 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function introduction(name){
+    if (name === "Josh") {
+    return ("Hi, my name is Josh.")
+    }else if (name === "Nancy"){
+    return ("Hi, my name is Nancy.")
+}
+}
+function introductionWithLanguage(name, language){
+    if (name === "Josh", language === "Ember.js.") {
+    return ("Hi, my name is Josh and I am learning to program in Ember.js.")
+    }else if (name === "Nancy", language === "React") {
+        return("Hi, my name is Nancy and I am learning to program in React")}
+}
+    
+function introductionWithLanguageOptional(name, language) {
+    if (name === "Gracie") {
+    return ("Hi, my name is Gracie and I am learning to program in JavaScript.")
+    } else if (language === "Python") {
+        return ("Hi, my name is Gracie and I am learning to program in Python.")
+    }
+}
